90b7949b975bb731cbdb930a0a92e4bb5ec02729
João Pedro Alves <jotalmeida007@hotmail.com>
2020-09-16 19:36:55 -0300

    Adicionar conexão com o banco e frontend

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import axios from 'axios';
+
+const api = axios.create({ baseURL: 'api' });
 
 export default function App() {
-  return <h1>Desafio Final do Bootcamp Full Stack</h1>;
+  const [transactions, setTransactions] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchTransactions = async () => {
+      const { data } = await api.get('/transaction?period=2019-07');
+      console.log(data);
+
+      setTransactions(data.transactions);
+    };
+
+    fetchTransactions();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Desafio Final do bootcamp</h1>
+
+      {transactions.map((transaction) => {
+        return <p key={transaction}>{transaction}</p>;
+      })}
+    </div>
+  );
 }
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,3 +2,21 @@ const express = require('express');
 const transactionRouter = express.Router();
 
 module.exports = transactionRouter;
+transactionRouter.get('/', async (request, response) => {
+  const { query } = request;
+
+  try {
+    if (!query.period) {
+      throw new Error(
+        `É necessário informar o parâmetro "period", cujo o valor deve estar nor formato yyyy-mm`
+      );
+    }
+    response.send({
+      lenght: 2,
+      transactions: ['transaction1', 'transaction2'],
+    });
+  } catch ({ message }) {
+    console, log(message);
+    response.status(400).send({ error: messege });
+  }
+});
